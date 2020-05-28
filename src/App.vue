410a4388d3d562dc13932f401be3f9e7642018e6
三井翔吾 <mitsui@atsu-maru.co.jp>
2020-05-28 19:23:10 +0900

    .

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,56 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>Metropolis–Hastings Algorithm Time-Lapse</h1>
+    <nav class="ui">
+      <button
+        class="btn-control"
+        @click="pause = !pause"
+      >{{ pause ? '実行する' : '停止する' }}</button>
+      <VueSlider
+        v-model="value"
+        :min="1"
+        :max="20"
+        :tooltip-formatter="val => `Speed: ${val}`"
+      />
+    </nav>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import VueSlider from 'vue-slider-component'
+import 'vue-slider-component/theme/antd.css'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    VueSlider,
+  },
+  data() {
+    return {
+      pause: false,
+      value: 10,
+    }
   }
 }
 </script>
 
 <style>
 #app {
+  max-width: 1200px;
+  margin: 0 auto;
+  padding: 32px 0;
+
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+}
+
+.ui {
+  display: flex;
+}
+.vue-slider {
+  flex-grow: 1;
+  margin-left: 16px;;
 }
 </style>
