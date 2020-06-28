0aea57e3cd6f6a37c5a38721b541071c754e62cd
Mitsui <todays.mitsui@gmail.com>
2020-06-29 01:56:58 +0900

    docs: さらにコメント追記

--- src/App.vue
+++ src/App.vue
@@ -87,9 +87,12 @@ export default class App extends Vue {
 
   // ======================================================================== //
 
-  /**
-   * 提案分布 - 正規分布からの実現値の組
-   */
+  /** 目標分布のカーネル */
+  p ({ x, y }: Point): number {
+    return Math.exp(-0.5 * (x * x - 2 * b * x * y + y * y))
+  }
+
+  /** 提案分布 - 正規分布からの実現値の組 */
   q ({ x, y }: Point): Point {
     return {
       x: x + rnorm(0, SIGMA),
@@ -97,26 +100,20 @@ export default class App extends Vue {
     }
   }
 
-  /**
-   * 目標分布のカーネル
-   */
-  p ({ x, y }: Point): number {
-    return Math.exp(-0.5 * (x * x - 2 * b * x * y + y * y))
-  }
-
-  /**
-   * サンプル取得処理
-   */
-  sample (count: number): Sample {
+  /** サンプル取得処理 */
+  sample (): Sample {
+    // 点 current 、初回は current === undefined なので { x0, y0 } を使う
     const current = this.current || { x: this.x0, y: this.y0 }
-    const next = this.q(current)
+    const next = this.q(current)      // 提案分布を使って点 next を選ぶ
 
-    const pCurrent = this.p(current)
-    const pNext = this.p(next)
+    const pCurrent = this.p(current)  // 点 current における目標分布の確率密度
+    const pNext = this.p(next)        // 点 next における目標分布の確率密度
 
-    const r = pNext / pCurrent // 受容確率
-    const accepted = r > 1 || r > uniform(0, 1)
-    const result = accepted ? 'accepted' : 'rejected'
+    const r = pNext / pCurrent        // 受容確率
+
+    // r が 1 以上ならそのまま受容する (accepted === true になる)
+    // r が 1 より小さいなら、確率 r で受容する
+    const accepted = r >= 1 || r > uniform(0, 1)
 
     console.table({
       current: `(${current.x.toFixed(2)}, ${current.y.toFixed(2)})`,
@@ -124,20 +121,31 @@ export default class App extends Vue {
       pCurrent: pCurrent.toFixed(2),
       pNext: pNext.toFixed(2),
       r: r.toFixed(2),
-      result
+      result: accepted ? 'accepted' : 'rejected'
     })
 
-    if (accepted) {
-      this.current = { ...next }
-    }
-
     return {
-      count,
-      burnin: count < BURN_IN_PERIOD,
+      count: this.count,
+      burnin: this.count < BURN_IN_PERIOD,
       current,
       next,
-      result
+      result: accepted ? 'accepted' : 'rejected'
     }
+  }
+
+  /** サンプル取得を繰り返す実行するためのループ */
+  loop () {
+    // サンプル取得
+    const sample = this.sample()
+
+    // 取得した sample が受容されていた場合は、次に備えて点 current を点 next で置き換える
+    if (sample.result === 'accepted') { this.current = sample.next }
+
+    this.samples.push(sample)
+    this.count += 1
+
+    // 1000 / speed ミリ秒だけ待ってから loop() を再び呼び出す
+    if (!this.pause) { setTimeout(this.loop, 1000 / this.speed) }
   }
 
   // ======================================================================== //
@@ -152,16 +160,6 @@ export default class App extends Vue {
       y: this.y0
     }
   }
-
-  loop () {
-    const sample = this.sample(this.count)
-    this.samples.push(sample)
-    this.count += 1
-
-    if (!this.pause) { setTimeout(this.loop, 1000 / this.speed) }
-  }
-
-  // ======================================================================== //
 
   onChangePause () {
     if (!this.pause) { this.loop() }
